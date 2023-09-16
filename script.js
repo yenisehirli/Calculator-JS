7b31c9ba6746d7629a478024c685ee9666f07bfd
Mustafa yenişehirli <you@example.com191805024@stu.adu.edu.tr>
2023-09-16 21:17:06 +0300

    feat : addEventListener

--- script.js
+++ script.js
@@ -8,3 +8,26 @@ updateDisplay();
 function updateDisplay() {
   display.value = displayValue;
 }
+
+keys.addEventListener('click', function(e) {
+    const element = e.target;
+
+    if (!element.matches('button')) return;
+
+    if (element.classList.contains('operator')) {
+        console.log('operator', element.value);
+        return;
+    }
+
+    if (element.classList.contains('decimal')) {
+        console.log('decimal', element.value);
+        return;
+    }
+    
+    if (element.classList.contains('clear')) {
+        console.log('clear', element.value);
+        return;
+    }
+
+    console.log('number',element.value);
+});    
